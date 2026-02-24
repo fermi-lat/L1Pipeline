879def7ce94ec9aa535b4b457b8f362f330fc887
omodei <nicola.omodei@gmail.com>
2026-02-24 11:44:11 -0800

    getting ready for deploiment

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+singularity exec --bind /sdf/group/fermi/a:/afs/slac/g/glast --bind /sdf:/sdf /sdf/group/fermi/sw/containers/fermi-rhel6.sif ./install.py
+#/sdf/home/g/glast/a/pipeline-II/prod/pipeline load -m DEV xml/L1ProcS3DF-6.1.xml
---
+++ preamble.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /sdf/group/fermi/a/applications/astroTools/astrotools_setup_s3df.sh
+echo "===============HELLO WORLD! HERE IS THE PREAMBLE!==============="
+
